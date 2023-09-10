ca72fd6c37e1b66d8f5056668a295ddb21e2857b
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-10 16:52:27 +0300

    Setup Spring

---
+++ jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/JpaspecificationsApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetaksunger.jpaspecifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaspecificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaspecificationsApplication.class, args);
+	}
+
+}
---
+++ jpaspecifications/src/test/java/com/ahmetaksunger/jpaspecifications/JpaspecificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetaksunger.jpaspecifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaspecificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
