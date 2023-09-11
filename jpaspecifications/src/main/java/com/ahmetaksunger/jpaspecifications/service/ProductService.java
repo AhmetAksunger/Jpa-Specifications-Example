c5f2540107c28761c6c6438b933ce410637f6e5f
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-11 17:53:34 +0300

    Implement getProducts method in ProductServiceImpl

--- jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/service/ProductService.java
+++ jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/service/ProductService.java
@@ -1,4 +1,11 @@
 package com.ahmetaksunger.jpaspecifications.service;
 
+import com.ahmetaksunger.jpaspecifications.dto.ListProductRequest;
+import com.ahmetaksunger.jpaspecifications.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts(ListProductRequest listProductRequest);
 }
--- jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/service/impl/ProductServiceImpl.java
+++ jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/service/impl/ProductServiceImpl.java
@@ -1,8 +1,22 @@
 package com.ahmetaksunger.jpaspecifications.service.impl;
 
+import com.ahmetaksunger.jpaspecifications.dto.ListProductRequest;
+import com.ahmetaksunger.jpaspecifications.entity.Product;
+import com.ahmetaksunger.jpaspecifications.repository.ProductRepository;
 import com.ahmetaksunger.jpaspecifications.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> getProducts(ListProductRequest listProductRequest) {
+        return productRepository.findAll(listProductRequest.toSpecification());
+    }
 }
