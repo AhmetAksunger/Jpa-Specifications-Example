19b2a4f66a1ea6aaea11ef3f0fb122aeb25a9059
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-11 17:53:51 +0300

    Create getProducts endpoint in ProductController

--- jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/controller/ProductController.java
+++ jpaspecifications/src/main/java/com/ahmetaksunger/jpaspecifications/controller/ProductController.java
@@ -1,9 +1,26 @@
 package com.ahmetaksunger.jpaspecifications.controller;
 
+import com.ahmetaksunger.jpaspecifications.dto.ListProductRequest;
+import com.ahmetaksunger.jpaspecifications.entity.Product;
+import com.ahmetaksunger.jpaspecifications.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(@RequestBody ListProductRequest listProductRequest) {
+        return ResponseEntity.ok(productService.getProducts(listProductRequest));
+    }
 }
